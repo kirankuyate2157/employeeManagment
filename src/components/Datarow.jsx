80a8fc2205167abb6043507ee80420ed5bfb5c4c
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-05-09 11:18:29 +0530

    edited

--- src/components/Datarow.jsx
+++ src/components/Datarow.jsx
@@ -1,45 +1,118 @@
-import React from "react";
-import { TbEdit } from "react-icons/tb";
-import { MdOutlineDelete } from "react-icons/md";
-const Datarow = () => {
-  return (
-    <>
-      <tr className='flex justify-between px-3 border text-md bg-gray-100 mt-[1px] mx-1 gap-2 rounded-lg items-center align-middle'>
-        <td className='font-normal  flex flex-wrap justify-center px-2 p-1 rounded w-[5%]'>
-          1
-        </td>
-        <td className='font-normal  overflow-auto px-2 p-1 rounded w-[15%]'>
-          <div className='overflow-x-auto whitespace-nowrap truncate'>
-            Kiran kuyate dvdfyvbhgvxgwxvgwvxghwvgh
-          </div>
-        </td>
-        <td className='font-normal  overflow-auto px-2 p-1 rounded w-[15%]'>
-          <div className='overflow-x-auto whitespace-nowrap truncate'>
-            Acme Inc.
-          </div>
-        </td>
+import React, { useState } from "react";
+import { TiEdit } from "react-icons/ti";
+import { MdDelete } from "react-icons/md";
 
-        <td className='font-normal  overflow-auto px-2 p-1 rounded w-[20%]'>
+const Datarow = ({ data, onUpdate }) => {
+  const {
+    id,
+    name: initialName,
+    company: initialCompany,
+    email: initialEmail,
+  } = data;
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+  const [company, setCompany] = useState(initialCompany);
+  const [email, setEmail] = useState(initialEmail);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+    onUpdate({ id, name, company, email });
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    // Reset the edited values back to their original values
+    setName(initialName);
+    setCompany(initialCompany);
+    setEmail(initialEmail);
+  };
+
+  return (
+    <tr className='flex justify-between px-3 border text-md bg-gray-100 mt-[1px] mx-1 gap-2 rounded-lg items-center align-middle'>
+      <td className='font-normal flex flex-wrap justify-center px-2 p-1 rounded w-[5%]'>
+        {id}
+      </td>
+      <td className='font-normal overflow-auto px-2 p-1 rounded w-[15%]'>
+        {isEditing ? (
+          <input
+            type='text'
+            className='w-full focus:outline-none'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        ) : (
           <div className='overflow-x-auto whitespace-nowrap truncate'>
-            john.doe@example.com
+            {name}
           </div>
-        </td>
-        <td className='font-normal   px-2 p-1 text-2xl  rounded w-[15%]'>
-          ********
-        </td>
-        <td className='font-normal  flex justify-center px-2  rounded  w-[15%]'>
-          <div className=' bg-green-200 text-green-600 items-center align-middle pb-1 rounded-xl px-4 text-sm'>
-            active
+        )}
+      </td>
+      <td className='font-normal overflow-auto px-2 p-1 rounded w-[15%]'>
+        {isEditing ? (
+          <input
+            type='text'
+            className='w-full focus:outline-none'
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
+        ) : (
+          <div className='overflow-x-auto whitespace-nowrap truncate'>
+            {company}
           </div>
-        </td>
-        <td className='font-normal  px-2 p-1 rounded w-[15%]'>
-          <div className='flex gap-4 justify-center text-xl'>
-            <TbEdit className='hover:cursor-pointer' />
-            <MdOutlineDelete className='hover:cursor-pointer' />
+        )}
+      </td>
+      <td className='font-normal overflow-auto px-2 p-1 rounded w-[20%]'>
+        {isEditing ? (
+          <input
+            type='text'
+            className='w-full focus:outline-none'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        ) : (
+          <div className='overflow-x-auto whitespace-nowrap truncate'>
+            {email}
           </div>
-        </td>
-      </tr>
-    </>
+        )}
+      </td>
+      <td className='font-normal px-2 p-1 text-2xl rounded w-[15%]'>
+        ********
+      </td>
+      <td className='font-normal flex justify-center px-2 rounded w-[15%]'>
+        <div className='bg-green-200 text-green-600 items-center align-middle pb-1 rounded-xl px-4 text-sm'>
+          active
+        </div>
+      </td>
+      <td className='font-normal px-2 p-1 rounded w-[15%]'>
+        {isEditing ? (
+          <div className='flex gap-4 justify-center'>
+            <button
+              className='hover:cursor-pointer text-blue-500'
+              onClick={handleSaveClick}
+            >
+              Save
+            </button>
+            <button
+              className='hover:cursor-pointer text-red-500'
+              onClick={handleCancelClick}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <div className='flex gap-4 justify-center'>
+            <TiEdit
+              className='hover:cursor-pointer'
+              onClick={handleEditClick}
+            />
+            <MdDelete className='hover:cursor-pointer' />
+          </div>
+        )}
+      </td>
+    </tr>
   );
 };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,47 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Datarow from "./Datarow";
 
+const data = [
+  {
+    id: 1,
+    name: "John Doe",
+    company: "Acme Inc.",
+    email: "john.doe@example.com",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    company: "XYZ Corp.",
+    email: "jane.smith@example.com",
+  },
+  // Add more data objects as needed
+];
+
 const Table = () => {
+  const [tableData, setTableData] = useState(data);
+
+  const handleUpdate = (updatedData) => {
+    const updatedTableData = tableData.map((item) => {
+      if (item.id === updatedData.id) {
+        return { ...item, ...updatedData };
+      }
+      return item;
+    });
+    setTableData(updatedTableData);
+  };
+
   return (
     <div className='w-full'>
       <table className='w-full flex flex-col'>
-        <thead className=' '>
-          <tr className='  flex flex-row  mx-1 py-1 border-b-[1px] border-gray-400 text-lg font-semibold gap-1 rounded-lg '>
-            <th className='font-semibold  w-[5%]'>ID</th>
-            <th className='font-semibold  w-[15%]'>Member Name</th>
-
-            <th className='font-semibold  w-[15%]'>Company Name</th>
-            <th className='font-semibold  w-[20%]'>Email</th>
-            <th className='font-semibold  w-[15%]'>Password</th>
-            <th className='font-semibold  w-[15%]'>status</th>
-            <th className='font-semibold  w-[15%]'>Actions</th>
+        <thead>
+          <tr className='flex flex-row mx-1 py-1 border-b-[1px] border-gray-400 text-lg font-semibold gap-1 rounded-lg'>
+            <th className='font-semibold w-[5%]'>ID</th>
+            <th className='font-semibold w-[15%]'>Member Name</th>
+            <th className='font-semibold w-[15%]'>Company Name</th>
+            <th className='font-semibold w-[20%]'>Email</th>
+            <th className='font-semibold w-[15%]'>Password</th>
+            <th className='font-semibold w-[15%]'>Status</th>
+            <th className='font-semibold w-[15%]'>Actions</th>
           </tr>
         </thead>
         <tbody className='max-h-screen overflow-y-auto'>
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
-          <Datarow />
+          {tableData.map((item) => (
+            <Datarow key={item.id} data={item} onUpdate={handleUpdate} />
+          ))}
         </tbody>
       </table>
     </div>
